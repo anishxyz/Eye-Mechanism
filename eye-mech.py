1bc24fe32aa86a2f3ab1276ecb2aecc6170e653a
anish <anishagrawal2003@gmail.com>
2022-08-03 15:53:02 -0700

    autodetect port on macOS!!!

--- eye-mech.py
+++ eye-mech.py
@@ -2,6 +2,7 @@ import sys, os
 import serial
 import time
 import traceback
+import glob
 
 speeds = {
         "fast": 3,
@@ -23,8 +24,9 @@ class eye_mech:
     def __init__(self, port, baudrate=9600, bytesize=8, parity=serial.PARITY_NONE,
                  timeout=2, stopbits=serial.STOPBITS_TWO, writeTimeout=2, inter_byte_timeout=2 ):
         if port.lower() == "auto-detect":
-            # auto-detect does not work on macOS currently :(
-            self.port = get_port_name("USB Serial Port")['port']
+            # uncomment for respective OS
+            self.port = glob.glob("/dev/tty.usbmodem*")[0]  # for macOS
+            # self.port = get_port_name("USB Serial Port")['port'] # for windows
         else:
             self.port = port
         self.baudrate = baudrate
@@ -36,6 +38,9 @@ class eye_mech:
         self.inter_byte_timeout = inter_byte_timeout
 
     def connect(self):
+        """
+        open port to start communication with eye-mech
+        """
         self.serialPort = serial.Serial(
             port=self.port, baudrate=self.baudrate, bytesize=self.bytesize, parity=self.parity, timeout=self.timeout,
             stopbits=self.stopbits, writeTimeout=self.writeTimeout, inter_byte_timeout=self.inter_byte_timeout)
@@ -43,22 +48,37 @@ class eye_mech:
         self.verbosity("off")
 
     def disconnect(self):
+        """
+        close port to end communication with eye-mech
+        :return:
+        """
         self.serialPort.close()
 
     def off(self):
         self.set_attenuation(255)
 
     def flush(self):
+        """
+        clear serialport input and output
+        """
         self.serialPort.flushOutput()
         self.serialPort.flushInput()
 
     def write_command(self, cmd):
+        """
+        Manually write command to the eye-mech
+        Adds \n as end-line marker
+        :param cmd: desired command to eye-mech
+        """
         cmd += "\n"
         for char in cmd:
             self.serialPort.write(char.encode('utf-8'))
             time.sleep(0.5)
 
     def read_output(self):
+        """
+        Return all unread output from serial port
+        """
         str_out = self.serialPort.readall()
         return str_out.decode()  # .split('\n\r')
 
@@ -118,8 +138,9 @@ class eye_mech:
 
 
 if __name__ == '__main__':
-    #arduino = eye_mech('auto-detect')
-    arduino = eye_mech('/dev/cu.usbmodem3134301')  # change port on re-plug
+    print(glob.glob("/dev/tty.usbmodem*")[0])
+    arduino = eye_mech('auto-detect')
+    # arduino = eye_mech('/dev/cu.usbmodem3134301')  # change port on re-plug
     arduino.connect()
     print("connect success!")
     arduino.orbit('fast', 1)
