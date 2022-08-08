e2e8bdb0ed3a16606489e3dd92c26b1f72238f73
anish <anishagrawal2003@gmail.com>
2022-08-08 15:17:31 -0700

    commenting...

--- eyeController/servoController.h
+++ eyeController/servoController.h
@@ -41,18 +41,52 @@ void orbit(int ms, int rotations);
  */
 void oscillate(int ms, int oscillations, boolean input);
 
-void backAndForth(int startPulse, int endPulse, int ms, int servoNum);
+//void backAndForth(int startPulse, int endPulse, int ms, int servoNum); //deprecated in favor of oscillate
 
+/**
+ * Set axis to degree position
+ *
+ * @param deg degree to be set
+ * @param servoNum axis to be controlled
+ */
 void setPosition(float deg, boolean servoNum);
 
+/**
+ * Increment axis by set amount of pulse-width steps
+ *
+ * @param axis axis of choice: true for x axis, false for y axis
+ * @param steps steps to be incremented
+ */
 void incrCoord(bool axis, int steps);
 
+/**
+ * Get current PWM position of x-axis servo motor
+ *
+ * @return integer pulse-width value of x motor
+ */
 int getPWMX();
 
+/**
+ * Get current PWM position of y-axis servo motor
+ *
+ * @return integer pulse-width value of y motor
+ */
 int getPWMY();
 
+/**
+ * Get degree value of current PWM position of servo motor
+ *
+ * @param xAxis axis of choice: true for x axis, false for y axis
+ * @return float value of curr position degrees
+ */
 float getPWMDeg(bool xAxis);
 
+/**
+ * Moves the selected axis by a certain number of degrees
+ *
+ * @param deg degrees to be moved
+ * @param xAxis axis of choice: true for x axis, false for y axis
+ */
 void movePosition(float deg, bool xAxis);
 
 #endif //EYE_MECHANISM_SERVOCONTROLLER_H
